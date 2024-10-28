71fe229f5e1974b568ec779d282fa1cdbf4b5d89
aeffront <a.effront@gmail.com>
2024-10-28 16:36:42 +0100

    lottie_update

--- js/lottie.js
+++ js/lottie.js
@@ -40,39 +40,57 @@ var animation_main = bodymovin.loadAnimation({
   animation_yellow.addEventListener('DOMLoaded', () => {
     const hitbox_yellow = document.getElementsByClassName("yellow").item(0);
     console.log(hitbox_yellow);
+    yellow_complete = true;
+    animation_yellow.onComplete = () => { yellow_complete = true; };
 
     hitbox_yellow.addEventListener('click', () => {
-      animation_yellow.goToAndPlay(0);
+      if(yellow_complete)animation_yellow.goToAndPlay(0);
+      yellow_complete = false;
+      
     });
 
     const hitbox_orange = document.getElementsByClassName("orange").item(0);
     console.log(hitbox_orange);
+    orange_complete = true;
+    animation_orange.onComplete = () => { orange_complete = true; };
 
     hitbox_orange.addEventListener('click', () => {
-      animation_orange.goToAndPlay(0);
+      if(orange_complete)animation_orange.goToAndPlay(0);
+      orange_complete = false;
     });
 
     const hitbox_green = document.getElementsByClassName("green").item(0);
     console.log(hitbox_green);
+    green_complete = true;
+    animation_green.onComplete = () => { green_complete = true; };
 
     hitbox_green.addEventListener('click', () => {
-      animation_green.goToAndPlay(0);
+      if(green_complete)animation_green.goToAndPlay(0);
+      green_complete = false;
+      
     });
 
     const hitbox_black = document.getElementsByClassName("black").item(0);
     console.log(hitbox_black);
+    black_complete = true;
+    animation_black.onComplete = () => { black_complete = true; };
 
     hitbox_black.addEventListener('click', () => {
-      animation_black.goToAndPlay(0);
+      if(black_complete)animation_black.goToAndPlay(0);
+      black_complete = false;
     });
 
     animation_black.play();
+    black_complete = false;
     setTimeout(() => {
       animation_green.play();
+      green_complete = false;
       setTimeout(() => {
         animation_orange.play();
+        orange_complete = false;
         setTimeout(() => {
           animation_yellow.play();
+          yellow_complete = false;
         }, 500);
       }, 500);
     }, 500);
